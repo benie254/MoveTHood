f70197453cd65b5c70873fc53af77d2d07a534a5
Benie <davinci.monalissa@gmail.com>
2022-06-18 02:33:17 +0300

    make minor update to app's root views

--- neighborhood/views.py
+++ neighborhood/views.py
@@ -9,23 +9,21 @@ from neighborhood.forms import UserUpdateForm, ProfileUpdateForm
 def home(request):
 	return render(request, 'hood/index.html')
 
+
 @login_required
 def profile(request):
 	title = 'Profile'
 	if request.method == 'POST':
 		update_user_form = UserUpdateForm(request.POST,instance=request.user)
 		update_profile_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
-
 		if update_user_form.is_valid() and update_profile_form.is_valid():
 			update_user_form.save()
 			update_profile_form.save()
 			messages.success(request,'Profile successfully updated!')
 			return redirect(to='profile')
-
 		else:
 			update_user_form = UserUpdateForm(instance=request.user)
 			update_profile_form = ProfileUpdateForm(instance=request.user.profile)
 		return render(request,'user/profile.html',{'update_user_form':update_user_form,'update_profile_form':update_profile_form})
-
 	return render(request,'user/profile.html',{"title":title})
 
