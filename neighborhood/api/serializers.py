ba38a89b5de971001fa014e1f8492bd7348f1a25
Benie <davinci.monalissa@gmail.com>
2022-06-21 04:18:19 +0300

    remove the unused serializers

--- neighborhood/api/serializers.py
+++ neighborhood/api/serializers.py
@@ -2,25 +2,11 @@ from rest_framework import serializers
 from neighborhood.models import Business,UserPost,Chama,HealthDept,PoliceDept
 
 
-class BizSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Business
-		fields = ('id','name','description','address','email','phone',)
-
-class PostSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = UserPost
-		fields = ('id','title','description','address')
-
-class ChamaSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Chama
-		fields = ('id','name','description','address')
-
 class PoliceSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = PoliceDept
 		fields = ('id','name','email','address','email','phone',)
+
 
 class HealthSerializer(serializers.ModelSerializer):
 	class Meta:
