3b8b4593dd9d94390e94c12ae6ce1fc14093702b
Benie <davinci.monalissa@gmail.com>
2022-06-21 11:39:11 +0300

    remove unused imports

--- neighborhood/api/serializers.py
+++ neighborhood/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from neighborhood.models import Business,UserPost,Chama,HealthDept,PoliceDept
+from neighborhood.models import HealthDept,PoliceDept
 
 
 class PoliceSerializer(serializers.ModelSerializer):
